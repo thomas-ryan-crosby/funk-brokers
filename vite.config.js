1bdde290d52ebab0aa0a0022b9426164d0f3e3b8
Thomas Ryan Crosby <thomas.ryan.crosby@gmail.com>
2026-02-01 09:02:47 -0600

    Revert "Switch Vite minifier to terser."
    This reverts commit fff64d7a95074e86c52b27335116312a4f11bb40.

--- vite.config.js
+++ vite.config.js
@@ -6,20 +6,10 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
     assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: undefined,
-      },
-    },
-    terserOptions: {
-      compress: {
-        module: false,
-        passes: 2,
-      },
-      format: {
-        comments: false,
       },
     },
   },
